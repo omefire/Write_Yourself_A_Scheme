e639ed55f4a768930008ee24cafbcb7b19d88bec
Omar Mefire <omefire@gmail.com>
2015-09-17 21:15:21 -0700

    Work In Progress: Simple parser. Getting introduced to Parsec

---
+++ simple_parser.hs
@@ -0,0 +1,25 @@
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+               | List [LispVal]
+               | DottedList [LispVal] LispVal
+               | Number Integer
+               | String String
+               | Bool Bool
+
+main :: IO ()
+main = do
+  (expr:_) <- getArgs
+  putStrLn (readExpr expr)
